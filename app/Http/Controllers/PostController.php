1eab9fb24bed3c6463e71a45d1f390812bd12b53
SamarMI <samar.mohamed.5523@gmail.com>
2021-03-21 14:07:54 +0200

    handle posts from db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,19 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
-    {
-        $allPosts = [
-            ['id' => 1, 'title' => 'laravel', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20'],
-            ['id' => 2, 'title' => 'PHP', 'posted_by' => 'Mohamed', 'created_at' => '2021-04-15'],
-            ['id' => 3, 'title' => 'Javascript', 'posted_by' => 'Ali', 'created_at' => '2021-06-01'],
-        ];
+    {$allPosts = Post::all(); //object of eloquent collection
 
+        return view('posts.index', [
+            'posts' => $allPosts
+        ]);
         return view('posts.index', [
             'posts' => $allPosts
         ]);

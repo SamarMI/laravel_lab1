92c6f256f421947ea65f33b2952180d68ac2a02f
SamarMI <samar.mohamed.5523@gmail.com>
2021-03-21 15:35:43 +0200

    handle create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+
+
+
 
 class PostController extends Controller
 {
@@ -21,24 +24,45 @@ class PostController extends Controller
     public function show($postId)
     {
         
-        $allPosts =[ ['id' => 1, 'title' => 'laravel', 'description' => 'laravel is awsome framework', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20','email'=>'Ahmed@gmai.com']
-                , ['id' => 2, 'title' => 'PHP', 'description' => 'php is native language ', 'posted_by' => 'Mohamed', 'created_at' => '2021-04-15','email'=>'Mohamed@gmai.com']
-                , ['id' => 3, 'title' => 'Javascript', 'description' => 'Javascript is native language ', 'posted_by' => 'Mohamed', 'created_at' => '2021-06-01','email'=>'Ali@gmai.com'],
-             ];
-           //  $post = ['id' => 1, 'title' => 'laravel', 'description' => 'laravel is awsome framework', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20'];
+        $post = Post::find($postId); //object of Post model
+
+        // $anotherSyntax = Post::where('id', $postId)->first();
+        //  $anotherSyntaxForSinglePost = Post::where('title', 'Laravel')->first(); //select * from posts where title = 'Laravel' limit 1;
+        // $anotherSyntaxGetAllPostsWithTitle = Post::where('title', 'Laravel')->get(); //select * from posts where title = 'Laravel';
+
         return view('posts.show', [
-            'post' => $allPosts[$postId -1],
+            'post' => $post,
         ]);
     }
 
     public function create()
     {
-        return view('posts.create');
+        return view('posts.create',[
+            'users' => User::all()
+        ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        //logic to insert request data into db
+        // $requestData = request()->all();
+        
+        //another syntax
+        // $title = request()->title;
+        // $description = request()->description;
+
+        $requestData = $request->all();
+        //dd($requestData);
+        /* Post::create([
+         'title' => $request->title,
+          'description' => $request->description,
+         ]);*/
+        Post::create($requestData);
+
+        //another syntax
+        // $post = new Post;
+        // $post->title = $request->title;
+        // $post->description = $request->description;
+        // $post->save();
 
         return redirect()->route('posts.index');
     }
